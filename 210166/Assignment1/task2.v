b08305d8774f19b28cf48000e75fa5b2cfb78121
anuj21S <106988736+anuj21S@users.noreply.github.com>
2022-07-21 21:15:38 +0530

    Corrected task2.v

--- 210166/Assignment1/task2.v
+++ 210166/Assignment1/task2.v
@@ -1,10 +1,10 @@
 module flip_flop(
-    input A, B, output reg C
+    input A, B, clk, output reg C
 );
     initial begin
         C = 1'b0;
     end
-    always @(A or B) begin
+    always @(posedge clk) begin
         C = (~A & ~B & C) | (A & ~B & ~C) | (A & B);
     end
 endmodule
