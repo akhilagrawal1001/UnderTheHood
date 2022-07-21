441f34944bfa83d853e71155996d94785856ca2e
anuj21S <106988736+anuj21S@users.noreply.github.com>
2022-07-21 21:17:23 +0530

    Corrected task2 test bench

--- 210166/Assignment1/task2_tb.v
+++ 210166/Assignment1/task2_tb.v
@@ -1,11 +1,17 @@
 `include "task2.v"
+`timescale 1ns/1ns
 module tb;
-    reg A,B;
+    reg A,B,clk;
     wire C;
-    flip_flop flipflop0 (A,B,C);
+    flip_flop flipflop0 (A,B,clk,C);
+    always begin
+        clk = ~clk;
+        #10;
+    end
+    
     initial begin
-        A=0;
-        B=0;
+        A=1'b0;
+        B=1'b0;
         #2;
         $monitor("A = %b, B = %b, C = %b",A,B,C);
         repeat(20) begin
